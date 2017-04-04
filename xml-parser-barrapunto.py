a525ae4dd28aa277c7a1c8f42e5d9734a5a76b2c
Sergio Cobos <SergioCobos@MacBook-Pro-de-Cobos.local>
2017-04-04 19:04:30 +0200

    Ejercicio realizado

---
+++ xml-parser-barrapunto.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+#
+# Simple XML parser for the RSS channel from BarraPunto
+# Jesus M. Gonzalez-Barahona
+# jgb @ gsyc.es
+# TSAI and SAT subjects (Universidad Rey Juan Carlos)
+# September 2009
+#
+# Just prints the news (and urls) in BarraPunto.com,
+#  after reading the corresponding RSS channel.
+
+from xml.sax.handler import ContentHandler
+from xml.sax import make_parser
+import sys
+from urllib import request
+
+class myContentHandler(ContentHandler):
+
+    def __init__ (self):
+        self.inItem = False
+        self.inContent = False
+        self.theContent = ""
+        self.line = ""
+
+    def startElement (self, name, attrs):
+        if name == 'item':
+            self.inItem = True
+        elif self.inItem:
+            if name == 'title':
+                self.inContent = True
+            elif name == 'link':
+                self.inContent = True
+
+    def endElement (self, name):
+        if name == 'item':
+            self.inItem = False
+        elif self.inItem:
+            if name == 'title':
+                # To avoid Unicode trouble
+                self.inContent = False
+                self.theContent = ""
+            elif name == 'link':
+                lnk = "<p><a href='" + self.theContent + "''>" + self.line + "</a></p><br/>"
+                print (lnk)
+                self.inContent = False
+                self.theContent = ""
+                self.line = ""
+
+    def characters (self, chars):
+        if self.inContent:
+            self.theContent = self.theContent + chars
+
+# --- Main prog
+"""Como lo sacamos de la url esto ya no hace falta"""
+#if len(sys.argv)<2:
+#    print ("Usage: python xml-parser-barrapunto.py <document>")
+#    print (" <document>: file name of the document to parse")
+#    sys.exit(1)
+""" Imprimimos el comienzo del html"""
+print("<!DOCTYPE html><html><body>")
+print("<ul>")
+# Load parser and driver
+theParser = make_parser()
+theHandler = myContentHandler()
+theParser.setContentHandler(theHandler)
+
+# Ready, set, go!
+url = "http://barrapunto.com/index.rss"
+xmlStream = request.urlopen(url)
+theParser.parse(xmlStream)
+print("</ul></body></html>")
+print ("Parse complete")
